95e53fe611a719757277318cb99fe880ac1b7966
alan-k-albin <alankalbin25@gmail.com>
2026-06-21 15:27:45 +0530

    Auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -89,13 +89,9 @@ export const login = async (req, res) => {
       }
     })
   } catch (error) {
-  console.error('Google login error:', error);
-  console.error('Message:', error.message);
-
-  res.status(500).json({
-    message: error.message
-  });
-}
+    console.error('Login error:', error)
+    res.status(500).json({ message: 'Server error' })
+  }
 }
 
 export const googleLogin = async (req, res) => {
